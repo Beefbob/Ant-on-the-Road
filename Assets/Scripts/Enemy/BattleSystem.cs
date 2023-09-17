d64b52d3ec177deffc534b08ecf66b35f4b81c1f
Beefbob <142109603+Beefbob@users.noreply.github.com>
2023-09-17 09:12:43 -0500

    Re-organizing files, getting things setup for enemies

---
+++ Assets/Scripts/Enemy/BattleSystem.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class BattleSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        StartBattle();
+    }
+
+    private void StartBattle()
+    {
+
+    }
+}
